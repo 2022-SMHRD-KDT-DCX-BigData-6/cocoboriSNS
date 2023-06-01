f8f528579249ea6b8c7c9fd627e04d4bc98aa389
learningfactory <learning@e>
2023-06-01 10:53:07 +0900

    회원관리 수정_신수정

--- cocoboriSNS/src/main/java/com/smhrd/controller/MemberDeleteService.java
+++ cocoboriSNS/src/main/java/com/smhrd/controller/MemberDeleteService.java
@@ -11,11 +11,12 @@ import javax.servlet.http.HttpSession;
 
 import com.smhrd.model.CocoMemberDAO;
 
+
 public class MemberDeleteService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		String user_email = request.getParameter("user_email");
 		System.out.println(user_email);
 		CocoMemberDAO dao = new CocoMemberDAO();
